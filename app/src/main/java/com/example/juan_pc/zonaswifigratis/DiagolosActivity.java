d10a49c0ad984df69b7b190256bc141e8877807e
juandredondo <juandredondo@gmail.com>
2017-10-13 19:16:13 -0500

    agregando imagenes

---
+++ app/src/main/java/com/example/juan_pc/zonaswifigratis/DiagolosActivity.java
@@ -0,0 +1,15 @@
+package com.example.juan_pc.zonaswifigratis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiagolosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogos);
+
+
+    }
+}
