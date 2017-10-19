9de22557bed9d5d4cbf77350237c79913e3bf6c8
juandredondo <juandredondo@gmail.com>
2017-10-19 12:19:04 -0500

    agregando librreria sweet dialog para cuadrod de dialogo mas agradables a la vista del usuario

--- app/src/main/java/com/example/juan_pc/zonaswifigratis/DiagolosActivity.java
+++ app/src/main/java/com/example/juan_pc/zonaswifigratis/DiagolosActivity.java
@@ -1,7 +1,9 @@
 package com.example.juan_pc.zonaswifigratis;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -12,10 +14,12 @@ import android.widget.Toast;
 import java.util.Arrays;
 import java.util.List;
 
+import cn.pedant.SweetAlert.SweetAlertDialog;
+
 public class DiagolosActivity extends AppCompatActivity {
 
     // vistas de tipo global
-     Button btnOpciones,btnDialogPersonalizado,btnPlay,btnPause;
+     Button btnOpciones,btnDialogPersonalizado,btnPlay,btnPause,btnDialogoSimple;
      TextView tv;
 
     //  declaracion  de todas las vistas que se van a usar en esta actividad
@@ -23,16 +27,6 @@ public class DiagolosActivity extends AppCompatActivity {
 //
 
 
-    // Boolean array for initial selected items
-
-    private  boolean[] checkedColors = new boolean[]{
-            false, // Red
-            false, // Green
-            false, // Blue
-            false, // Purple
-            false // Olive
-
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,20 +39,35 @@ public class DiagolosActivity extends AppCompatActivity {
         //seteo de las diferentes vistas que se usan en esta actividad
         tv=(TextView)findViewById(R.id.tvOpciones);
         btnDialogPersonalizado=(Button)findViewById(R.id.btnPersonalizacion);
+
         btnPlay =(Button) findViewById(R.id.btnPlay) ;
         btnPause=(Button) findViewById(R.id.btnPause) ;
+        btnDialogoSimple=(Button) findViewById(R.id.btnDialogoSimple);
 
         final  MediaPlayer media= MediaPlayer.create(DiagolosActivity.this,R.raw.audio);
 
 
 
-
 //---------------------------------------------------------------------------------------------------------
         //haciendo un dialogo modal mostrando opciones de tipo lista
+        // Boolean array for initial selected items
 
-        View.OnClickListener onClickListener = new View.OnClickListener() {
+
+        btnOpciones.setOnClickListener  (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+
+                // colocar esta  arreglo como  private y globl para que se concerven los check seleccionados cada ves que se ejecute
+                 final boolean[] checkedColors = new boolean[]{
+                        false, // Red
+                        false, // Green
+                        false, // Blue
+                        false, // Purple
+                        false // Olive
+
+                };
+
 
                 // String array for alert dialog multi choice items
                 String[] colors = new String[]{
@@ -105,8 +114,6 @@ public class DiagolosActivity extends AppCompatActivity {
                             boolean checked = checkedColors[j];
                             if (checked) {
                                 tv.setText(tv.getText() + colorsList.get(j) + "\n");
-                            } else {
-                                tv.setText("");
                             }
                         }
                     }
@@ -125,11 +132,7 @@ public class DiagolosActivity extends AppCompatActivity {
                 });
                 alert2.show();//se muestra el  dialogo
             }
-
-
-        };
-
-
+        });
 
         //-----------------------------------------------------------------------------------------------------------------------
         //AGREGANDO EL LISTENER AL BOTON PARA DIALOGOS PERSONALIZADOS
@@ -137,7 +140,7 @@ public class DiagolosActivity extends AppCompatActivity {
         btnDialogPersonalizado.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // agregando un alert dialog  personalizado, es decir con la ayuda de un layout
                 //1. declaramos el objeto de la clase AlertDialog.Builder
                 AlertDialog.Builder myBuilder = new AlertDialog.Builder(DiagolosActivity.this);
 
@@ -149,23 +152,56 @@ public class DiagolosActivity extends AppCompatActivity {
                 dialog.show();// agregado comentario
 
             }
+
         });
 
-//---------------------------------------------------------------------
+//--------------------------------------------------------------------
 //        agregando reproducio audio  or fi
         //creamos el objeto de tipo media plaer
-        btnPlay.setOnClickListener(new View.OnClickListener() { @Override public void onClick(View v) { media.start(); } });
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v)
+            {
+                media.start();
+            } });
+
+        // boton de pause
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 media.pause();//
-                juan();
+
             }
         });
 
+        //----------------------------------------------------------------
+
+        //agregando alert dialog simple
+        btnDialogoSimple.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //creando un alert dialog simple son sweet alert
+                new SweetAlertDialog(DiagolosActivity.this, SweetAlertDialog.WARNING_TYPE)
+                        .setTitleText("Are you sure?")
+                        .setContentText("Won't be able to recover this file!")
+                        .setConfirmText("Yes,delete it!")
+                        .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                            @Override
+                            public void onClick(SweetAlertDialog sweetAlertDialog) {
+
+                                Toast.makeText(DiagolosActivity.this, "realizando aciones ", Toast.LENGTH_SHORT).show();
+                                sweetAlertDialog
+                                        .setTitleText("Deleted!")
+                                        .setContentText("Your imaginary file has been deleted!")
+                                        .setConfirmText("OK")
+                                        .setConfirmClickListener(null)
+                                        .changeAlertType(SweetAlertDialog.SUCCESS_TYPE);
+                            }
+                        })
+                        .show();
+            }
+        });//******************************-=================----------------
+
     }
 
-    public  void juan (){
 
-    }
 }
